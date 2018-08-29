c06a1ee4ae702afd6448158e6f992f8b942b3a6a
Pencher zZ <nageshangzei@gmail.com>
2018-08-29 11:56:43 +0800

    Update 1.3.go

--- Go-Challenge/1.3.go
+++ Go-Challenge/1.3.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-     "fmt"
-	   "strconv"
-	   "encoding/hex"
-	   )
+    "fmt"
+    "strconv"
+    "encoding/hex"
+)
 
 func main() {
-  // 1
-	i := 12345
-	// 10 to 16
-	h := strconv.FormatInt(int64(i), 16)
-  fmt.Println(h)
+    // 1
+    i := 12345
+    // 10 to 16
+    h := strconv.FormatInt(int64(i), 16)
+    fmt.Println(h)
   
-	// 16 to byte
-  var dest []byte
-	fmt.Sscanf(h, "%X", &dest)
-	fmt.Println(string(dest))
+    // 16 to byte
+    var dest []byte
+    fmt.Sscanf(h, "%X", &dest)
+    fmt.Println(string(dest))
 	
-	// second method
-	s, _ := hex.DecodeString(h)
-	fmt.Printf("%s",s)
-    
-  // 2
-	s1 := "3.1415926"
-	f,_ := strconv.ParseFloat(s1, 64)
-	fmt.Printf("%T value is %v",f, f)
-
+    // second method
+    s, _ := hex.DecodeString(h)
+    fmt.Printf("%s",s)
+    // 2
+    s1 := "3.1415926"
+    f,_ := strconv.ParseFloat(s1, 64)
+    fmt.Printf("%T value is %v",f, f)
 }
