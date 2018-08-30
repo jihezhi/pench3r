bd63efac7acd88d1c57adfde72aba085e4fc79e5
Pencher zZ <nageshangzei@gmail.com>
2018-08-30 13:03:02 +0800

    Update 1.7.go

--- Go-Challenge/1.7.go
+++ Go-Challenge/1.7.go
@@ -2,12 +2,12 @@
 
 package main
 
-import (	
+import (
 	"fmt"
 )
 
 // 建立from和to channel之间的关系以及方向
-func cc(from,to chan int) {
+func cc(to,from chan int) {
     to <- 1 + <- from
 }
 
@@ -17,15 +17,15 @@ func main() {
     // 保存用于出结果的channel，相当于尾部
     var src chan int = dstorg
     var dst chan int = dstorg
-	  for i := 0;i<10000;i++ {
-        // 创建channel用完不需要保存，因为通过goroutine进行管道之间管理的维持
-        // 最终只需要这一串channel的头和尾即可
+    for i := 0;i<10000;i++ {
+              // 创建channel用完不需要保存，因为通过goroutine进行管道之间管理的维持
+              // 最终只需要这一串channel的头和尾即可
 	      src = make(chan int)
-		    go cc(src, dst)
-		    dst = src
-	  }
+	      go cc(dst, src)
+	      dst = src
+    }
     // 在头部的channel中放入数据
     go func(c chan int) {c <- 4}(src)
     // 直接就在尾部的channel中获取到经过多成管道处理的结果
-	  fmt.Println(<-dstorg)
+    fmt.Println(<-dstorg)
 }
