1c1739a5f1adeb40b026854dc89e138c4a9a4752
Pencher zZ <nageshangzei@gmail.com>
2018-08-31 11:00:20 +0800

    Update 2.1.1.go
    去掉了全局变量，使用for来遍历channel

--- Go-Challenge/2.1.1.go
+++ Go-Challenge/2.1.1.go
@@ -7,24 +7,21 @@ import (
     "strconv"
 )
 
-var requestSender chan int
-var responseReceiver chan int
-var sumChannel chan int
-var sum int
-
 func main() {
     requestSender := make(chan int)
     responseReceiver := make(chan int)
     sumChannel := make(chan int)
+    var sum int
     // 初始化源管道中的数据
     go func() { sumChannel <- 0}()
     go func() {
-        for {
-            sum = <-sumChannel + <-requestSender
+        for s := range sumChannel {
+            sum = s + <-requestSender
             responseReceiver <- sum
             go func(s int) {sumChannel <- s}(sum)
         }
     } ()
+
     http.HandleFunc("/getValue", func(w http.ResponseWriter, r *http.Request) {
         // 获取querystring，并将其转化为int类型
         s := r.URL.Query()["int"][0]
