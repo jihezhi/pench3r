078e138d680dd2d9da76e674e61b87b69bb164bb
Pencher zZ <nageshangzei@gmail.com>
2018-08-31 18:11:50 +0800

    Create 2.3.go

---
+++ Go-Challenge/2.3.go
@@ -0,0 +1,37 @@
+// 粗糙的一版
+package main
+
+import (
+    "fmt"
+)
+
+type eval_desc_I interface {
+    evaluate() int
+    describe() string
+}
+
+type myType struct {
+    age int
+    name string
+}
+
+func (mt myType) evaluate() int {
+    return mt.age
+}
+
+func (mt myType) describe() string {
+    return mt.name
+}
+
+func multitype_show(typename ...eval_desc_I) {
+    for _,v := range typename {
+        fmt.Println(v.evaluate(), v.describe())
+    }
+}
+
+func main() {
+    mt := myType{18, "pench3r"}
+    mt1 := myType{19, "pench3r"}
+    mt2 := myType{20, "pench3r"}
+    multitype_show(mt, mt1, mt2)
+}
