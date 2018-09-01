57b955ec9f0fbd7772bb459557af6d0ab5f6999e
Pencher zZ <nageshangzei@gmail.com>
2018-09-01 21:38:17 +0800

    Update 2.3.go

--- Go-Challenge/2.3.go
+++ Go-Challenge/2.3.go
@@ -3,6 +3,7 @@ package main
 
 import (
     "fmt"
+    "errors"
 )
 
 type eval_desc_I interface {
@@ -23,20 +24,29 @@ func (mt myType) describe() string {
     return mt.name
 }
 
-func multitype_show(typename ...eval_desc_I) {
+func multitype_show(typename ...interface{}) error {
     if len(typename) == 0 {
-        fmt.Println("you need pass one eval_desc_I interface at least")
-        return
+        return errors.New("you need pass one eval_desc_I interface at least")
     }
     for _,v := range typename {
-        fmt.Println(v.evaluate(), v.describe())
+        switch t := v.(type) {
+            case myType:
+                fmt.Println(t, v.(myType).evaluate(), v.(myType).describe())
+            default:
+                return fmt.Errorf("you pass type is %T,need pass type of implement eval_desc_I interface", t)
+        }
     }
+    return nil
 }
+
 
 func main() {
     mt := myType{18, "pench3r"}
     mt1 := myType{19, "pench3r"}
     mt2 := myType{20, "pench3r"}
     multitype_show(mt, mt1, mt2)
-    multitype_show()
+    err := multitype_show(1)
+    if err != nil {
+        fmt.Println(err)
+    }
 }
