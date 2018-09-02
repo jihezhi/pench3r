9bc08d36aa3fcebeb8d39aeec422097b2c507344
Pencher zZ <nageshangzei@gmail.com>
2018-09-02 22:22:23 +0800

    Update 3.2.go
    修复广播功能，程序基本架构确定

--- Go-Challenge/3.2.go
+++ Go-Challenge/3.2.go
@@ -1,4 +1,4 @@
-// 未完成的一个简单demo,需要再调试一下
+// 整体的程序框架基本已经定型
 package main
 
 import (
@@ -11,17 +11,18 @@ import (
 func handleConn(c net.Conn) (chan string, chan string) {
     requestChannel := make(chan string)
     responseChannel := make(chan string)
+    tmp := make([]byte, 256)
     go func() {
-        tmp := make([]byte, 256)
         for {
-            _, err := c.Read(tmp)
+            n, err := c.Read(tmp)
             if err != nil {
                 if err != io.EOF {
                     fmt.Println("read error", err)
+                    c.Close()
+                    break
                 }
-                break
             }
-            requestChannel <- string(tmp)
+            requestChannel <- c.RemoteAddr().String() + string(tmp)
         }
         c.Close()
     }()
@@ -44,23 +45,27 @@ func main() {
         log.Fatal("net listen: ", err)
     }
     globalMessageChannel := make(chan string)
+    storeResponseChannel := make([]chan string, 0)
     for {
         conn, err := ln.Accept()
         if err != nil {
             log.Fatal("net accept: ", err)
         }
         req,res := handleConn(conn)
+        storeResponseChannel = append(storeResponseChannel, res)
         go func(req chan string) {
             for r := range req {
                 globalMessageChannel <- r
                 fmt.Printf("Recv msg: %v", r)
             }
         }(req)
-        go func(res chan string) {
+        go func() {
             for g := range globalMessageChannel {
-                res <- g
                 fmt.Printf("broadcast msg: %v", g)
+                for _,c := range storeResponseChannel {
+                    c <- g
+                }
             }
-        }(res)
+        }()
     }
 }
