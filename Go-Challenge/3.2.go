6722a33ec48b9801603284e26d9c4104a618a650
Pencher zZ <nageshangzei@gmail.com>
2018-09-03 16:12:05 +0800

    Update 3.2.go
    增加用户姓名，以及历史聊天内容保存和展示，后续添加处理连接的错误信息

--- Go-Challenge/3.2.go
+++ Go-Challenge/3.2.go
@@ -1,17 +1,36 @@
 // 整体的程序框架基本已经定型
+
 package main
 
 import (
     "fmt"
+	"math/rand"
     "net"
     "log"
     "io"
 )
 
-func handleConn(c net.Conn) (chan string, chan string) {
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// 用于随机生成用户姓名
+func randName(n int) string {
+	name := make([]byte, n)
+	for i := range name {
+		name[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(name)
+}
+
+func handleConn(c net.Conn, historyContent []string) (chan string, chan string) {
     requestChannel := make(chan string)
     responseChannel := make(chan string)
+	username := randName(6)
     tmp := make([]byte, 256)
+	// first write history content
+	for _,msg := range historyContent {
+		c.Write([]byte(msg))
+	}
+	// for loop to recive user input
     go func() {
         for {
             n, err := c.Read(tmp)
@@ -22,10 +41,11 @@ func handleConn(c net.Conn) (chan string, chan string) {
                     break
                 }
             }
-            requestChannel <- c.RemoteAddr().String() + string(tmp)
+            requestChannel <- username + ": " + string(tmp[:n])
         }
         c.Close()
     }()
+	// for loop to write response
     go func() {
         for {
             if v, ok := <-responseChannel; ok {
@@ -45,13 +65,16 @@ func main() {
         log.Fatal("net listen: ", err)
     }
     globalMessageChannel := make(chan string)
-    storeResponseChannel := make([]chan string, 0)
+    // 用于保存每个了解的responsechannel
+	var storeResponseChannel []chan string
+    // 用于保存所有用户发送过的信息
+	var historyContent []string
     for {
         conn, err := ln.Accept()
         if err != nil {
             log.Fatal("net accept: ", err)
         }
-        req,res := handleConn(conn)
+        req,res := handleConn(conn, historyContent)
         storeResponseChannel = append(storeResponseChannel, res)
         // fan-in
         go func(req chan string) {
@@ -63,6 +86,7 @@ func main() {
         // fan-out
         go func() {
             for g := range globalMessageChannel {
+				historyContent = append(historyContent, g)
                 fmt.Printf("broadcast msg: %v", g)
                 for _,c := range storeResponseChannel {
                     c <- g
