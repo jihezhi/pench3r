d052369515860f36e0ff261d80c1d119cc00b45f
Pencher zZ <nageshangzei@gmail.com>
2018-09-04 11:08:34 +0800

    Update 3.2.go
    凑活能跑起来，后续再使用websocket来实现一版

--- Go-Challenge/3.2.go
+++ Go-Challenge/3.2.go
@@ -1,5 +1,5 @@
-// 整体的程序框架基本已经定型
-
+// 解决了几个关键的问题，现在可以正常跑，但是写起来确实比较麻烦
+// 接着再使用websocket来实现一下
 package main
 
 import (
@@ -12,7 +12,12 @@ import (
 
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-// 用于随机生成用户姓名
+type ChatMessage struct {
+	username string
+	content string
+	conn_state bool
+}
+
 func randName(n int) string {
 	name := make([]byte, n)
 	for i := range name {
@@ -21,41 +26,57 @@ func randName(n int) string {
 	return string(name)
 }
 
-func handleConn(c net.Conn, historyContent []string) (chan string, chan string) {
-    requestChannel := make(chan string)
+func handleConn(c net.Conn, historyContent []string) (string, chan ChatMessage, chan string) {
+    requestChannel := make(chan ChatMessage)
     responseChannel := make(chan string)
 	username := randName(6)
     tmp := make([]byte, 256)
+	message := ChatMessage{username, username + " enter\n", true}
 	// first write history content
 	for _,msg := range historyContent {
 		c.Write([]byte(msg))
 	}
 	// for loop to recive user input
     go func() {
+		requestChannel <- message
         for {
             n, err := c.Read(tmp)
             if err != nil {
                 if err != io.EOF {
                     fmt.Println("read error", err)
-                    c.Close()
                     break
                 }
             }
-            requestChannel <- username + ": " + string(tmp[:n])
+			// if send msg len is 0, exit and close conn
+			if n == 0 {
+				break
+			}
+			message.content = username + ": " + string(tmp[:n])
+            requestChannel <- message
         }
+		message.conn_state = false
+		message.content = message.username + " exited\n"
+		requestChannel <- message
         c.Close()
     }()
 	// for loop to write response
     go func() {
         for {
+			if !message.conn_state {
+				break
+			}
             if v, ok := <-responseChannel; ok {
-                c.Write([]byte(v))
+                _, err := c.Write([]byte(v))
+				if err != nil {
+					break
+				}
             } else {
-                return
+				break
             }
         }
+		close(responseChannel)
     }()
-    return requestChannel, responseChannel
+    return username, requestChannel, responseChannel
 }
 
 func main() {
@@ -64,20 +85,19 @@ func main() {
     if err != nil {
         log.Fatal("net listen: ", err)
     }
-    globalMessageChannel := make(chan string)
-    // 用于保存每个了解的responsechannel
-	var storeResponseChannel []chan string
-    // 用于保存所有用户发送过的信息
+    globalMessageChannel := make(chan ChatMessage)
+	var storeRespChan map[string]chan string
+	storeRespChan = make(map[string]chan string)
 	var historyContent []string
     for {
         conn, err := ln.Accept()
         if err != nil {
             log.Fatal("net accept: ", err)
         }
-        req,res := handleConn(conn, historyContent)
-        storeResponseChannel = append(storeResponseChannel, res)
+        uname,req,res := handleConn(conn, historyContent)
+		storeRespChan[uname] = res
         // fan-in
-        go func(req chan string) {
+        go func(req chan ChatMessage) {
             for r := range req {
                 globalMessageChannel <- r
                 fmt.Printf("Recv msg: %v", r)
@@ -86,10 +106,13 @@ func main() {
         // fan-out
         go func() {
             for g := range globalMessageChannel {
-				historyContent = append(historyContent, g)
-                fmt.Printf("broadcast msg: %v", g)
-                for _,c := range storeResponseChannel {
-                    c <- g
+				historyContent = append(historyContent, g.content)
+                fmt.Printf("broadcast msg: %v", g.content)
+				if !g.conn_state {
+					delete(storeRespChan, g.username)
+				}
+                for _,c := range storeRespChan {
+					c <- g.content
                 }
             }
         }()
